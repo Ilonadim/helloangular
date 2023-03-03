1c8943125cfb5e0bb3995f963aa26c4a048463e1
dim <ilona.demchyshyna@gmail.com>
2023-03-03 13:27:23 +0200

    Dawn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  template: `<label>Введіть імя:</label>
+  <input [(ngModel)]="name" placeholder="name">
+  <h1>Привіт, {{name}}!</h1>`
 })
+
 export class AppComponent {
-  title = 'helloangular';
+  name = '';
 }
